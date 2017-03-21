6e3524ca0e2a31a5c39954ec1f0de7c32e84127a
JacobWylie <jacobwylie@gmail.com>
2017-03-21 18:49:26 +0100

    pipelines and whatnot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# treehouse-gulp-course
-Code for my Treehouse Gulp.js course!  June, 2015.
+<img src="https://github.com/gulpjs/artwork/blob/master/gulp.png?raw=true" alt="gulp logo">
+<h1>:beers: More Gulp Stuff :fries:</h1>
+<br>
+<h4>:computer: <a href="https://github.com/contra/gulp-concat">gulp-concat:</a> $npm install gulp-concat --save-dev</h4>
+<h4>:book: <a href="https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-tasks-in-series.md">Async tasks in gulp, from the gulp docs</a></h4>
+<h4>:book: <a href="https://github.com/gulpjs/gulp/blob/master/docs/API.md#async-task-support">Task Dependencies</a></h4>
+<h4>:book: <a href="https://github.com/isaacs/node-glob">Gulp uses the globbing pattern defined in the popular node-glob module</a></h4>
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 "use strict";
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps');
 
-gulp.task("concatScripts", function() {
-    gulp.src([
+gulp.task("concatScripts", () => {
+    return gulp.src([
         'js/jquery.js',
         'js/sticky/jquery.sticky.js',
         'js/main.js'
@@ -19,21 +19,48 @@ gulp.task("concatScripts", function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task("minifyScripts", function() {
-	gulp.src("js/app.js")
+gulp.task("minifyScripts", ['concatScripts'], () => {
+	return gulp.src("js/app.js")
 		.pipe(uglify())
 		.pipe(rename('app.min.js'))
 		.pipe(gulp.dest('js'));
 });
 
-gulp.task('compileSass', function() {
-  gulp.src("scss/application.scss")
+gulp.task('compileSass', () => {
+  return gulp.src("scss/application.scss")
       .pipe(maps.init())
       .pipe(sass())
       .pipe(maps.write('./'))
       .pipe(gulp.dest('css'));
 })
 
-gulp.task("default", ["hello"], function() {
-    console.log("the default task!!!!");
+gulp.task('watchSass', () => {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+
 });
+
+gulp.task('build', ['minifyScripts', 'compileSass']);
+
+gulp.task("default", ['build']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
